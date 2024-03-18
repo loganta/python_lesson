2183744fd9c6028116825028abdb87c0da7b9991
PhuongTa <phuong.ta@merkle.com>
2024-03-18 22:31:28 +0700

    lesson 2

---
+++ lesson2/lesson2.py
@@ -0,0 +1,12 @@
+mylist = []
+a = 0
+b = 10
+while a<=10:
+	mylist.append(a)
+	a += 1
+print('Display value in array mylist:')
+print('Sum no value of array mylist: %d', b)
+
+print('Values of mylist:')
+for i in mylist:
+	print(i) 
